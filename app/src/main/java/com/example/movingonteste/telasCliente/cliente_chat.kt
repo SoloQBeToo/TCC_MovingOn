f091063d0592b3c0095f1efb3528cb2428c9e512
SoloQBeToo <robertoandrade.13123@gmail.com>
2022-10-25 21:46:42 -0300

    dale

---
+++ app/src/main/java/com/example/movingonteste/telasCliente/cliente_chat.kt
@@ -0,0 +1,12 @@
+package com.example.movingonteste.telasCliente
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movingonteste.R
+
+class cliente_chat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cliente_chat)
+    }
+}
